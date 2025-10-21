f2803b23af168ff3e66ca577ef868de88b2f98f9
minhto28 <minhto28.dev@gmail.com>
2025-10-21 11:47:46 +0700

    up

--- crawl/crawl.go
+++ crawl/crawl.go
@@ -17,8 +17,8 @@ import (
 )
 
 const (
-	minTime = "16/10/2025"
-	maxTime = "20/10/2025" //Nếu bằng ngày hôm nay tức là đã crawl
+	minTime = "20/10/2025"
+	maxTime = "21/10/2025" //Nếu bằng ngày hôm nay tức là đã crawl
 )
 
 var grade = "g8"
