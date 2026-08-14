eced76edc7dbc13d0fb94c5537c43631b3e56534
minhto28 <minhto28.dev@gmail.com>
2026-08-14 18:10:19 +0700

    up

--- crawl/crawl.go
+++ crawl/crawl.go
@@ -17,8 +17,8 @@ import (
 )
 
 const (
-	minTime = "08/08/2026"
-	maxTime = "11/08/2026"
+	minTime = "11/08/2026"
+	maxTime = "14/08/2026"
 )
 
 var grade = "g8"
--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	
 	crawl.CrawlMath()
 	crawl.CrawlTVHL()
-	crawl.CrawlVideo()
+	//crawl.CrawlVideo()
 
 	// cutoff := time.Date(2023, 12, 1, 0, 0, 0, 0, time.UTC)
 	// err1 :=	crawl.ClearMath(cutoff)
